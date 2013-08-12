91498ab20f1180d6951f161b1c8c2ab4e47688de
SFantasy <forever.fantasy27@gmail.com>
2013-08-13 00:02:27 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #Filename: install.sh
 
 echo "Welcome to use Gmail-cli"
-echo "Checking..."
+echo "Checking...(this may need your root password)"
 
 #If you have curl installed
 which curl > /dev/null 2>&1
@@ -14,3 +14,9 @@ fi
 
 echo "Setting up your Gmail account..."
 read -p 'Input your username:' username
+
+sudo chmod 775 gmail.sh
+#Link it
+ln ./gamil.sh /bin/gmail
+
+echo 'Gmail-cli insalled.'
