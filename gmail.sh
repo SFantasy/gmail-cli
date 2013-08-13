8c5e11ca6dbb62a8d51e8ab793409e60b55b5b79
SFantasy <forever.fantasy27@gmail.com>
2013-08-13 23:05:24 +0800

    Add variable support "-n"

--- gmail.sh
+++ gmail.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #FileName: gmail.sh
 
+#Modify your username here
 username="forever.fantasy27@gmail.com"
 
 SHOW_COUNT=5
 
-echo 'Welcome to use gmail-cli'
-
+echo 'Gmail-cli'
+echo '------'
+if [ "$1" == "-n" -a -n "$2" ]; then
+	SHOW_COUNT=$2
+elif [ "$1" == "-n" -a -z "$2" ]; then
+	echo 'How many mails do you want to see?'
+	echo '(input number after "-n".)'
+	exit 0
+elif [ -z "$1" -a -z "$2" ]; then
+	echo '5 mails shown as default:'
+fi
 #Log in your Gmail 
 curl -u $username  --silent "https://mail.google.com/mail/feed/atom" | \
 tr -d '\n' | \
